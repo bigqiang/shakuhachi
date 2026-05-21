396922a5feb7735fc66507a82942fbd9ed002cd6
bigqiang <bigqiang@msn.com>
2026-05-21 15:03:47 +0800

    更改部署路径

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// 原 base: '/shakuhachi/' 改为‘/’ 以适合域名 http://shakuhachi.bigqiang.cn/ 的部署
 export default defineConfig({
   plugins: [react()],
-  base: '/shakuhachi/',
+  base: '/',
 })
